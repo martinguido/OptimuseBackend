9cfffb25433301319b320fe460a1a68649ed7332
Martin Guido <guidomartin7@gmail.com>
2024-02-20 21:01:35 -0300

    create controller

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from typing import Union
-
 from fastapi import FastAPI
-
-from DAO.Dao import Dao
-from DAO.Asset import Asset
-from DAO.AssetEnergyDemand import AssetEnergyDemand
-from DAO.AssetEnergyOutput import AssetEnergyOutput
-from DAO.AssetEnergySystem import AssetEnergySystem
-from DAO.EnergySystem import EnergySystem
-from DAO.EnergyType import EnergyType
+from controller.assets_controller import router
 
 app = FastAPI()
-file_path = "json_database.json"
-dao = Dao(file_path)
-energy_systems, assets, asset_energy_demands, asset_energy_outputs, energy_types, asset_energy_systems = dao.load_data()
+
+app.include_router(router)
 
 
-@app.get("/")
-def read_root():
-    print(energy_systems, assets, asset_energy_demands,
-          asset_energy_outputs, energy_types, asset_energy_systems)
-    return energy_systems
+@app.get("/health")
+def health():
+    return {"status": "OK"}
